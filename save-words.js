fa731e64abc085ecd3b0cea6938e61c958ea96bf
skimberk1 <skimberk1@gmail.com>
2014-09-22 07:11:51 -0400

    First commit. Added list of common words.

---
+++ save-words.js
@@ -0,0 +1,26 @@
+// To be used on
+// https://www.englishclub.com/vocabulary/common-words-5000.htm
+
+var holder = document.querySelector('.tableLRT10 tbody tr td ol');
+var elements = holder.getElementsByTagName('li');
+
+var words = '';
+
+for(var i = 0, len = elements.length; i < len; ++i) {
+	var word = elements[i].textContent;
+
+	if(word.length > 3) {
+		words += word + "\n";
+	}
+}
+
+var textBlob = new Blob([words], {type:'text/plain'});
+
+var downloadLink = document.createElement("a");
+downloadLink.download = 'words.txt';
+downloadLink.innerHTML = 'Download words';
+
+downloadLink.href = window.URL.createObjectURL(textBlob);
+document.body.appendChild(downloadLink);
+
+downloadLink.click();
